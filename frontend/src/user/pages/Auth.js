33ceeb23966d79503cde59f4c48dbd08c286fea9
SANRAJ BANOKAR <43671095+sanrajbanokar@users.noreply.github.com>
2023-04-08 12:35:39 +0530

    backend done

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext } from "react";
 import Input from "../../shared/components/FormElements/Input";
 
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import Card from "../../shared/components/UIElements/Card";
 import {
   VALIDATOR_EMAIL,
@@ -11,10 +13,15 @@ import { useForm } from "../../shared/hooks/form-hook";
 import "./Auth.css";
 import Button from "../../shared/components/FormElements/Button";
 import { AuthContext } from "../../shared/context/auth-context";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const Auth = (props) => {
   const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
+
+  //   const [isLoading, setIsLoading] = useState(false);
+  //   const [error, setError] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -32,31 +39,42 @@ const Auth = (props) => {
 
   const authSubmitHandler = async (event) => {
     event.preventDefault();
-    
-    if(isLoginMode) {
 
-    }else {
-        try{
-            const response = await fetch('http://localhost:5000/api/users/signup', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    name: formState.inputs.name.value,
-                    email: formState.inputs.email.value,
-                    password: formState.inputs.password.value
-                })
-            });
-
-            const responseData = await response.json();
-            console.log(responseData);
-        }catch(err) {
-            console.log(err);
-        }
+    if (isLoginMode) {
+      try {
+        await sendRequest(
+          "http://localhost:5000/api/users/login",
+          "POST",
+          JSON.stringify({
+            // name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+          {
+            "Content-Type": "application/json",
+          }
+        );
+        auth.login();
+      } catch (err) {}
+    } else {
+      try {
+        await sendRequest(
+          "http://localhost:5000/api/users/signup",
+          "POST",
+          JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+          {
+            "Content-Type": "application/json",
+          }
+        );
+        auth.login();
+      } catch (err) {
+        
+      }
     }
-
-    auth.login();
   };
 
   const switchModeHandler = () => {
@@ -84,48 +102,52 @@ const Auth = (props) => {
   };
 
   return (
-    <Card className="authentication">
-      <h2>{isLoginMode ? "Login Required" : "Sign Up"}</h2>
-      <form onSubmit={authSubmitHandler}>
-        {!isLoginMode && (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h2>{isLoginMode ? "Login Required" : "Sign Up"}</h2>
+        <form onSubmit={authSubmitHandler}>
+          {!isLoginMode && (
+            <Input
+              element="input"
+              id="name"
+              type="text"
+              label="Your Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Please enter a name."
+              onInput={inputHandler}
+            />
+          )}
           <Input
+            id="email"
             element="input"
-            id="name"
-            type="text"
-            label="Your Name"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a name."
+            label="E-Mail"
+            placeholder="E-Mail"
+            type="email"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid e-mail address."
             onInput={inputHandler}
           />
-        )}
-        <Input
-          id="email"
-          element="input"
-          label="E-Mail"
-          placeholder="E-Mail"
-          type="email"
-          validators={[VALIDATOR_EMAIL()]}
-          errorText="Please enter a valid e-mail address."
-          onInput={inputHandler}
-        />
-        <Input
-          id="password"
-          element="input"
-          label="Password"
-          type="password"
-          placeholder="Password"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid password (atleast 5 characters)."
-          onInput={inputHandler}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLoginMode ? "Login" : "Sign Up"}
+          <Input
+            id="password"
+            element="input"
+            label="Password"
+            type="password"
+            placeholder="Password"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid password (atleast 5 characters)."
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLoginMode ? "Login" : "Sign Up"}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          {isLoginMode ? "Not a user? SignUp" : "Already a user? Login"}
         </Button>
-      </form>
-      <Button inverse onClick={switchModeHandler}>
-        {isLoginMode ? "Not a user? SignUp" : "Already a user? Login"}
-      </Button>
-    </Card>
+      </Card>
+    </React.Fragment>
   );
 };
 
--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,28 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import UsersList from '../components/UsersList';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 const Users = () => {
-    const USERS = [
-        {
-            id: 'u1',
-            name:"Sanraj Banokar",
-            image:"https://static01.nyt.com/images/2020/12/14/well/14google-photo/14google-photo-videoSixteenByNineJumbo1600.jpg",
-            places: 3
-        },
-        {
-            id: 'u2',
-            name:"Shivangi Goyal",
-            image:"https://static01.nyt.com/images/2020/12/14/well/14google-photo/14google-photo-videoSixteenByNineJumbo1600.jpg",
-            places: 2
-        },
-        {
-            id: 'u3',
-            name:"Rohit Dumbey",
-            image:"https://static01.nyt.com/images/2020/12/14/well/14google-photo/14google-photo-videoSixteenByNineJumbo1600.jpg",
-            places: 1
-        },
-    ];
-    return <UsersList items={USERS} />
+    // const USERS = [
+    //     {
+    //         id: 'u1',
+    //         name:"Sanraj Banokar",
+    //         image:"https://static01.nyt.com/images/2020/12/14/well/14google-photo/14google-photo-videoSixteenByNineJumbo1600.jpg",
+    //         places: 3
+    //     },
+    //     {
+    //         id: 'u2',
+    //         name:"Shivangi Goyal",
+    //         image:"https://static01.nyt.com/images/2020/12/14/well/14google-photo/14google-photo-videoSixteenByNineJumbo1600.jpg",
+    //         places: 2
+    //     },
+    //     {
+    //         id: 'u3',
+    //         name:"Rohit Dumbey",
+    //         image:"https://static01.nyt.com/images/2020/12/14/well/14google-photo/14google-photo-videoSixteenByNineJumbo1600.jpg",
+    //         places: 1
+    //     },
+    // ];
+
+    // const [isLoading, setIsLoading] = useState(false);
+    // const [error, setError] = useState();
+    const [loadedUsers, setLoadedUsers] = useState();
+
+    const {isLoading, error, sendRequest, clearError} = useHttpClient();
+
+
+    useEffect(()=> {
+        const fetchRequest = async()=> {
+            try{
+                const responseData = await sendRequest('http://localhost:5000/api/users');
+                setLoadedUsers(responseData.users);
+            }catch(err){
+               
+            }  
+        };
+        fetchRequest();
+    },[sendRequest]);
+
+    return (
+        <React.Fragment>
+            <ErrorModal error ={error} onClear={clearError}/>
+    {isLoading && <div className="center">
+        <LoadingSpinner asOverlay/>
+    </div>}
+    {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+    );
+
 };
 
 export default Users;
